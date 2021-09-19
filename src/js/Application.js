f776b5b57fbbc4b65a4458f78813d009781fa090
Donyo Donev <donevdonyo39@gmail.com>
2021-09-19 11:47:02 +0300

    fixed spring animation problem

--- src/js/Application.js
+++ src/js/Application.js
@@ -1,16 +1,26 @@
 import EventEmitter from "eventemitter3";
 import anime from "animejs";
 export default class Application extends EventEmitter {
-  static get events() {
-    return {
-      READY: "ready",
-    };
-  }
+    static get events() {
+        return {
+            READY: "ready",
+        };
+    }
 
-  constructor() {
-    super();
-    this.init();
-    this.emit(Application.events.READY);
-  }
-  init() {}
+    constructor() {
+        super();
+        this.init();
+        this.emit(Application.events.READY);
+    }
+    init() {
+        document.querySelector('.article').addEventListener('click', () => {
+            anime({
+                targets: '.article',
+                translateX: 250,
+                direction: 'alternate',
+                loop: true,
+                ease: 'spring(1, 80, 10, 0)'
+            });
+        })
+    }
 }
